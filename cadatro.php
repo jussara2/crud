d25dde596b4b894db9bbbca7007809181bd869fe
jussara2 <144683878+jussara2@users.noreply.github.com>
2024-02-29 14:56:22 -0300

    l1

---
+++ cadatro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="box">
+        <form action="">
+            <fieldset>
+                <legend><b>Fórmulário de Clientes</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" nome="nome" id="none" class="inputUser" required>
+                    <label for="nome" class
+</body>
+</html>
