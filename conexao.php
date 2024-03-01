79259c6349491fac53e4bd9bf5198ebed4ae358f
jussara2 <144683878+jussara2@users.noreply.github.com>
2024-03-01 16:57:26 -0300

    01

--- conexao.php
+++ conexao.php
@@ -6,7 +6,7 @@ $bd = 'siatema';
 
 
 $conexao = new mysqli($host,$user,$senha,$bd);
-    if($conexao){
+    if(!$conexao){
         echo'erro ao se comunicar com o bd' .mysqli_connect_error;
     }
     else{
